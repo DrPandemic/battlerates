9e8eb43d80561bbd513c1be2ff62b1b2f16d3509
DrPandemic <bipbip500@gmail.com>
2018-02-11 14:58:53 -0500

    Adds crawler docker config and removes python dotenv

--- crawler/app.py
+++ crawler/app.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv, find_dotenv
 import os
 from battlerite_client.client import Client
 from src.crawler import Crawler
-
-load_dotenv(find_dotenv())
 
 client = Client(os.environ['battlerite_key'])
 crawler = Crawler(client, "real database", 10)
