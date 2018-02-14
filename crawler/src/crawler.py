b498ce7ef963a70cda9a116aadac0747fbcc2bb6
DrPandemic <bipbip500@gmail.com>
2018-02-13 20:58:02 -0500

    Sleeps during the loop

--- crawler/src/crawler.py
+++ crawler/src/crawler.py
@@ -1,4 +1,6 @@
 import logging
+import time
+import sys
 from enum import Enum
 from battlerite_client.client import Match, Response
 
@@ -11,12 +13,16 @@ class Crawler:
         """
         self.client = client
         self.rate = rate
+        self.alive = True
 
     def start(self) -> None:
         """
         Starts the infinite loop. It will call run at every tick.
         """
-        self.run()
+        while self.alive:
+            time.sleep(60 / self.rate)
+            self.run()
+            sys.stdout.flush()
 
     def run(self) -> None:
         """
@@ -26,7 +32,7 @@ class Crawler:
         if response.success:
             self.save_matches(response.parse())
         else:
-            logging.warning("Match query failed with: %s", response.raw.json())
+            logging.warning("Match query failed with: %s", response.raw.text)
 
     def save_matches(self, matches: [Match]) -> None:
         """
